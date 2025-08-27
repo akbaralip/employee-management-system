b2aa7569dac233e013948a5334174f6ac3007260
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2025-08-27 13:16:37 +0530

    Added three new apps

--- backend/ems_backend/urls.py
+++ backend/ems_backend/urls.py
@@ -3,8 +3,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('form/', include('forms.urls')),
-    path('auth/', include('authentication.urls')),
-    path('employ/', include('employees.urls')),
-
 ]
