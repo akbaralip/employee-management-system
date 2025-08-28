63bd7293d5eb3cf1a10e1a5de70329e2c22e6ebb
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2025-08-28 11:39:14 +0530

    User Auth

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password_confirm', 'first_name', 'last_name')
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
